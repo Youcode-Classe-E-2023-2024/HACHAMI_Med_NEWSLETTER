51191948491f9a40d4faf0c3ea9cdeb6b1847117
MedHachami <moha.hach.9@gmail.com>
2024-02-09 14:22:10 +0100

    laratrust admin panel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AuthMailingController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\EditorController;
+
+use App\Mail\MyTestEmail;
+use Illuminate\Support\Facades\Mail;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +22,12 @@ use App\Http\Controllers\AuthController;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('auth.login');
 });
 
+Route::get('/home', function () {
+    return view('auth.login');
+});
 //Authentication
 
 Route::get('/login',[AuthController::class,'loginG'])->name('login');
@@ -26,22 +36,35 @@ Route::post('/login',[AuthController::class,'loginP'])->name('loginUser');
 Route::get('/register',[AuthController::class,'registerG']);
 Route::post('/register',[AuthController::class,'registerP'])->name('register');
 
+Route::get('/forget-password',[AuthMailingController::class,'showForgetPassword'])->name('forget-password');
+Route::post('/forget-password',[AuthMailingController::class,'ForgetPassword']);
+
+Route::get('/reset/{token}',[AuthMailingController::class,'showResetPassword']);
+Route::post('/reset/{token}',[AuthMailingController::class,'ResetPassword'])->name('reset');
+
 
 // Admin Routes
 Route::group(['middleware' => 'custom.admin'], function () {
-    Route::get('/admin/dashboard', function () {
-        return "admin dashboard";
-    });
+    Route::get('/admin/dashboard',[AdminController::class,'index']);
+
+    Route::get('/logout' , [AuthController::class,'logout']);
 });
 
 // Editor Routes
 Route::group(['middleware' => 'custom.editor'], function () {
-    Route::get('/editor/dashboard', function () {
-        return "editor dashboard";
-    });
+    Route::get('/editor/dashboard',[EditorController::class,'index']);
+
+
+    Route::get('/logout' , [AuthController::class,'logout']);
+
 });
 
 
 Route::get('/forbidden',function (){
     return 'hhh';
 });
+
+
+
+
+
