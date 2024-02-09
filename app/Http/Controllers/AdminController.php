c3374e62f8f78461cdf87d2b0bc609c2a1e270e0
MedHachami <moha.hach.9@gmail.com>
2024-02-09 23:30:32 +0100

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class AdminController extends Controller
 {
     //
 
     public function index(){
-        return view('admin.index');
+        return Inertia::render('admin/Dashboard');
     }
+
 }
