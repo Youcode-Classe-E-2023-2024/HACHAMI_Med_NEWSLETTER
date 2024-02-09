1f24ba8d1a4c2450aebb38e8d88139a35067606b
MedHachami <moha.hach.9@gmail.com>
2024-02-09 23:27:39 +0100

    Create Dashboard.jsx

---
+++ resources/js/Pages/admin/Dashboard.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { router , usePage , Link  } from '@inertiajs/react'
+import { InertiaLink } from '@inertiajs/inertia-react';
+
+import { Inertia } from '@inertiajs/inertia'
+
+export default function Dashboard( ) {
+    // const { errors } = usePage().props
+
+    return (
+        <div>
+            Admin
+            <InertiaLink href="/logout"  className="underline">Logout</InertiaLink>
+        </div>
+    )
+}
+
+
