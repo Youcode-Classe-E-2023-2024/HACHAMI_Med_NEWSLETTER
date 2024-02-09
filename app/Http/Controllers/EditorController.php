f0ae969a65e08d738cdbdc5702fb336f0036780b
MedHachami <moha.hach.9@gmail.com>
2024-02-09 23:30:43 +0100

    Update EditorController.php

--- app/Http/Controllers/EditorController.php
+++ app/Http/Controllers/EditorController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class EditorController extends Controller
 {
     public function index(){
-        return view('editor.index');
+        return Inertia::render('editor/Dashboard');
     }
 }
