97e37c0023b7cdc6f7ed0d33fc1e9f7fa7f92848
MedHachami <moha.hach.9@gmail.com>
2024-02-15 15:15:26 +0100

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\template;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -10,17 +12,49 @@ class AdminController extends Controller
     //
 
     public function index(){
+        $members = User::whereHasRole('member')->select('id', 'email')->get();
+
+
+        $memberCount = User::whereHas('roles', function ($query) {
+            $query->where('name', 'member');
+        })->count();
+
+
+        $templateCount = template::whereNull('deleted_at')->get()->count();
+//        dd($templateCount);
+
+
+
+//        $templateCount = template::
         return Inertia::render('admin/Layout', [
             'Route'=>'dashboard',
             'pageName'=>'Main Dashboard',
+            'members'=>$members,
+            'memberCount'=>$memberCount,
+            'templateCount'=>$templateCount
         ]);
     }
 
+    public function deleteUsers(Request $request){
+        $usersToDelete = $request->json()->all();
 
-    public function about(){
+        foreach ($usersToDelete as $user) {
+            User::where('id', $user['id'])->delete();
+        }
+
+        return to_route('admin_dashboard');
+
+
+    }
+
+
+    public function templates(){
+        $templates = template::with('media')->get();
+
         return Inertia::render('admin/Layout', [
-            'Route'=>'about',
-            'pageName'=>'About',
+            'Route'=>'templates',
+            'pageName'=>'Templates',
+            'templates'=>$templates,
         ]);
     }
 
