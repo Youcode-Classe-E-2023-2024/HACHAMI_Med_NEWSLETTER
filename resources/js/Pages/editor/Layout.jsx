e5f0dd950e584ee9b5b99471c44e027f3dcab4c9
MedHachami <moha.hach.9@gmail.com>
2024-02-15 15:16:10 +0100

    Create Layout.jsx

---
+++ resources/js/Pages/editor/Layout.jsx
@@ -0,0 +1,27 @@
+import SideBar from "./components/SideBar.jsx";
+import Main from "./components/Main.jsx";
+import TemplateForm from "./components/TemplateForm.jsx";
+import React from "react";
+import Dashboard from "../admin/Dashboard.jsx";
+import About from "../admin/About.jsx";
+
+
+export default function Layout({Route,pageName , templates ,success , errors ,file  }){
+
+    const routeComponents = {
+        dashboard: <Main templates={templates} />,
+        create: <TemplateForm status={success} />,
+        // Add more routes as needed
+    };
+
+    const Child = routeComponents[Route] || null;
+
+    return(
+        <div className="bg-[#fdba74]">
+
+            <SideBar />
+            {Child}
+
+        </div>
+    )
+}
